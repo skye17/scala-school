9261ec0372b5fb445e36b62a8158840330d00fc7
skye17 <skye1795@gmail.com>
2016-09-04 21:30:29 +0300

    List and General BST tasks

--- src/main/scala/lectures/collections/MyListImpl.scala
+++ src/main/scala/lectures/collections/MyListImpl.scala
@@ -1,5 +1,7 @@
 package lectures.collections
 
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
 /**
   * Представим, что по какой-то причине, Вам понадобилась своя обертка над списком целых чисел List[Int]
   *
@@ -17,26 +19,75 @@ package lectures.collections
   */
 object MyListImpl extends App {
 
-  case class MyList(data: List[Int]) {
+  case class MyList[T, M <: Iterable[T]](data: Iterable[T]) {
 
-    def flatMap(f: (Int => MyList)) =
-      MyList(data.flatMap(inp => f(inp).data))
-
-    //
-    def map(f: (Int => Int)): MyList = flatMap(inp => MyList(List(f(inp))))
+    def flatMap(f: (T => MyList[T, M])): MyList[T, Iterable[T]] =
+      new MyList(data.flatMap(inp => f(inp).data))
 
     //
-    def foldLeft(acc: Int)(f: ((Int, Int)) => Int): Int = data match {
+    def map(f: (T => T)): MyList[T, Iterable[T]] = flatMap(inp => new MyList(List(f(inp))))
+
+    //
+    def foldLeft(acc: T)(f: ((T, T)) => T): T = data match {
       case Nil => acc
-      case head::tail => MyList(tail).foldLeft(f(acc, head))(f)
+      case head::tail => new MyList(tail).foldLeft(f(acc, head))(f)
     }
 
-    def filter(f: (Int => Boolean)) = flatMap( x => if (f(x)) MyList(List(x)) else MyList(List()))
+    def filter(f: (T => Boolean)): MyList[T, Iterable[T]] = flatMap(x => if (f(x)) new MyList(List(x)) else new MyList(List()))
   }
 
-  require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
+
+  class MyListBuffer[T](override val data:ListBuffer[T]) extends MyList(data)
+
+  class MyIndexedList[T](data:IndexedSeq[T]) extends MyList(data)
+
+  /*require(MyList(List(1, 2, 3, 4, 5, 6)).map(_ * 2).data == List(2, 4, 6, 8, 10, 12))
   require(MyList(List(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
   require(MyList(List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
-  require(MyList(Nil).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+  require(MyList(Nil).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)*/
 
+  require(MyList[Int, List[Int]](List(1, 2, 3, 4, 5, 6)).map(p => p * 2).data == List(2, 4, 6, 8, 10, 12))
+  require(MyList[Long, ListBuffer[Long]](ListBuffer(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(MyList[Int, List[Int]](List(1, 2, 3, 4, 5, 6)).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 21)
+  require(MyList[Float, IndexedSeq[Float]](ArrayBuffer.empty[Float]).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+
+
+  require(new MyListBuffer[Long](ListBuffer(1, 2, 3, 4, 5, 6)).filter(_ % 2 == 0).data == List(2, 4, 6))
+  require(new MyIndexedList[Float](ArrayBuffer.empty[Float]).foldLeft(0)((tpl) => tpl._1 + tpl._2) == 0)
+}
+
+trait Numeric[T] {
+  def add(x: T, y: T): T
+}
+
+
+
+
+
+object NumericExperiment extends App{
+  implicit val int2Num: Numeric[Int] = new Numeric[Int]() {
+    override def add(x: Int, y: Int): Int = x + y
+  }
+
+  def doAdd[T: Numeric](one: T, other: T) = {
+    val r = implicitly[Numeric[T]]
+    println("Added " + r.add(one, other))
+  }
+
+  def doAddWithExplicitPrm[T](one: T, other: T)(implicit evidence: Numeric[T]) = {
+    println("Added " + evidence.add(one, other))
+  }
+
+  case class MoneyAmount(money: Double)
+
+  implicit val money2Num: Numeric[MoneyAmount] = new Numeric[MoneyAmount]() {
+    override def add(x: MoneyAmount, y: MoneyAmount): MoneyAmount = MoneyAmount(x.money +
+      y.money)
+  }
+
+
+  doAdd(MoneyAmount(100), MoneyAmount(300))
+
+  //doAdd(1, 2)
+  //doAddWithExplicitPrm(1, 2)
 }
--- src/main/scala/lectures/oop/types/GeneralBST.scala
+++ src/main/scala/lectures/oop/types/GeneralBST.scala
@@ -1,5 +1,12 @@
 package lectures.oop.types
 
+
+import scala.math.Ordering.{Float, String}
+
+import lectures.matching.SortingStuff.Watches
+
+import scala.util.Random
+
 /**
   * Модифицируйте реализацию BSTImpl из предыдущего задания.
   * Используя тайп параметры и паттерн Type Class реализуте GeneralBSTImpl таким образом,
@@ -13,23 +20,95 @@ package lectures.oop.types
   * * * * Watches из задачи SortStuff. Большими считаються часы с большей стоимостью
   */
 
-trait GeneralBST {
-  //val value: T
-  val left: Option[GeneralBST]
-  val right: Option[GeneralBST]
+trait GeneralBST[T] {
+  val value: T
+  val left: Option[GeneralBST[T]]
+  val right: Option[GeneralBST[T]]
 
-  def add(newValue: Int): GeneralBST
+  def add(newValue: T): GeneralBST[T]
 
-  def find(value: Int): Option[GeneralBST]
+  def traverseBFS:List[List[T]]
+
+  def find(value: T): Option[GeneralBST[T]]
 }
 
-class GeneralBSTImpl extends GeneralBST {
+case class GeneralBSTImpl[T](value:T, left: Option[GeneralBST[T]] = None,
+                             right: Option[GeneralBST[T]] = None)(implicit val ordering: Ordering[T]) extends GeneralBST[T] {
   //override val value = _
+  implicit  val o = scala.math.Ordering[Float]
 
-  override def find(value: Int): Option[GeneralBST] = ???
+  override def find(value: T): Option[GeneralBST[T]] = if (traverseBFS.exists(level => level.contains(value))) Some(this) else None
 
-  override def add(newValue: Int): GeneralBST = ???
+  def addImpl(newValue: T): GeneralBSTImpl[T] =  {
+    if (ordering.compare(newValue,value) < 0) {
+      left match {
+        case Some(tree:GeneralBST[T]) => GeneralBSTImpl(value, Some(tree.add(newValue)), right)
+        case None => GeneralBSTImpl(value, Some(GeneralBSTImpl(newValue)),right)
+      }
+    }
+    else if (ordering.compare(newValue,value) > 0) {
+      right match {
+        case Some(tree:GeneralBST[T]) => GeneralBSTImpl(value, left, Some(tree.add(newValue)))
+        case None => GeneralBSTImpl(value, left,Some(GeneralBSTImpl(newValue)))
+      }
+    } else this
+  }
 
-  override val left: Option[GeneralBST] = ???
-  override val right: Option[GeneralBST] = ???
+  override def add(newValue:T):GeneralBST[T] = addImpl(newValue)
+
+
+  def traverseBFS:List[List[T]] = {
+    def merge(a:List[List[T]], b:List[List[T]], acc:List[List[T]] = Nil):List[List[T]] =
+      (a, b) match {
+        case (Nil,Nil) => acc
+        case (head::_, Nil) => acc ++ a
+        case (Nil, head::_) => acc ++ b
+        case (leftHead::leftTail, rightHead::rightTail) => merge(leftTail, rightTail, acc :+ (leftHead ++ rightHead))
+      }
+    (left, right) match {
+      case (None,None) => List(List(value))
+      case (Some(tree), None) => List(value) +: tree.traverseBFS
+      case (None, Some(tree)) => List(value) +: tree.traverseBFS
+      case (Some(leftTree), Some(rightTree)) => List(value) +: merge(leftTree.traverseBFS, rightTree.traverseBFS)
+
+    }
+  }
+
+  //override val left: Option[GeneralBST] = ???
+  //override val right: Option[GeneralBST] = ???
+
+}
+
+object GeneralTreeTest extends App {
+  val maxValue = 5000
+  implicit val watchOrdering:Ordering[Watches] = Ordering.by((w:Watches) => w.cost)
+
+
+  def floatTreeGenerator(numNodes:Int): GeneralBST[Float] = {
+    val treeValues = for ( i <- 1 until numNodes) yield (Math.random()* maxValue).toFloat
+    treeValues.foldLeft(GeneralBSTImpl((Math.random()*maxValue).toFloat))((tree:GeneralBSTImpl[Float], newVal:Float) => tree.addImpl(newVal))
+  }
+
+  val floatTree = floatTreeGenerator(5)
+  val strTree = stringTreeGenerator(5)
+  val watchTree = watchTreeGenerator(3)
+  println(floatTree)
+  println
+  println(strTree)
+  println
+  println(watchTree)
+  println
+
+  def stringGenerator(n:Int):String = Random.alphanumeric.take(n).mkString
+
+  def stringTreeGenerator(numNodes:Int): GeneralBST[String] = {
+    val treeValues = for ( i <- 1 until numNodes) yield stringGenerator(5)
+    treeValues.foldLeft(GeneralBSTImpl(stringGenerator(5)))((tree:GeneralBSTImpl[String], newVal:String) => tree.addImpl(newVal))
+  }
+
+  def watchTreeGenerator(numNodes:Int): GeneralBST[Watches] = {
+    val treeValues = for ( i <- 1 until numNodes) yield Watches(stringGenerator(10), (Math.random()*maxValue).toFloat)
+    treeValues.foldLeft(GeneralBSTImpl(Watches(stringGenerator(10), (Math.random()*maxValue).toFloat)))((tree:GeneralBSTImpl[Watches], newVal:Watches) => tree.addImpl(newVal))
+  }
+
 }
